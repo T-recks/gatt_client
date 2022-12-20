d7f4a5aa06a06e8aff65a4d44ea71cae699070cb
T-recks <timjrecker@gmail.com>
2022-12-19 16:31:22 -0800

    toggle wifi connection using button

--- main/gattc_demo.c
+++ main/gattc_demo.c
@@ -60,6 +60,11 @@
 // LED Config
 #define BLINK_GPIO CONFIG_BLINK_GPIO
 
+// Button Config
+#define BUTTON_GPIO 0
+#define GPIO_INPUT_PIN_SEL (1ULL<<BUTTON_GPIO)
+#define ESP_INTR_FLAG_DEFAULT 0
+static QueueHandle_t gpio_evt_queue = NULL;
 
 /* FreeRTOS event group to signal when we are connected & ready to make a request */
 static EventGroupHandle_t wifi_event_group;
@@ -209,7 +214,7 @@ static esp_err_t event_handler(void *ctx, system_event_t *event)
     case SYSTEM_EVENT_STA_GOT_IP:
         ESP_LOGI(TAG, "esp32 got IP");
         wifi_connect = true;
-        set_wifi_status(true);
+        // set_wifi_status(true);
         break;
     case SYSTEM_EVENT_STA_DISCONNECTED:
         ESP_LOGI(TAG, "esp32 disconnected from Wifi");
@@ -717,10 +722,56 @@ static void esp_gattc_cb(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if, esp
     } while (0);
 }
 
+static void IRAM_ATTR gpio_isr_handler(void* arg)
+{
+    uint32_t gpio_num = (uint32_t) arg;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+static void gpio_task_example(void* arg)
+{
+    uint32_t io_num;
+    for(;;) {
+        if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+            printf("GPIO[%"PRIu32"] intr, val: %d\n", io_num, gpio_get_level(io_num));
+            set_wifi_status(!s_led_state);
+        }
+    }
+}
+
+void gpio_init(void) {
+    gpio_config_t io_conf = {};
+    //interrupt of rising edge
+    io_conf.intr_type = GPIO_INTR_POSEDGE;
+    //bit mask of the pins, use GPIO0 here
+    io_conf.pin_bit_mask = GPIO_INPUT_PIN_SEL;
+    //set as input mode
+    io_conf.mode = GPIO_MODE_INPUT;
+    //enable pull-up mode
+    io_conf.pull_up_en = 1;
+    gpio_config(&io_conf);
+
+    //change gpio interrupt type for one pin
+    gpio_set_intr_type(BUTTON_GPIO, GPIO_INTR_NEGEDGE);
+
+    //create a queue to handle gpio event from isr
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    //start gpio task
+    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
+
+    //install gpio isr service
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(BUTTON_GPIO, gpio_isr_handler, (void*) BUTTON_GPIO);
+}
+
 void app_main(void)
 {
     // Initialize LED
     configure_led();
+
+    // Initialize GPIO
+    gpio_init();
 
     // Initialize NVS.
     esp_err_t ret = nvs_flash_init();
